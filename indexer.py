d45fa5f48b67f95f9b7538085386317c3896e78d
shachar58 <38552118+shachar58@users.noreply.github.com>
2020-11-24 13:34:14 +0200

    indexer changes for part A

--- indexer.py
+++ indexer.py
@@ -14,17 +14,27 @@ class Indexer:
         """
 
         document_dictionary = document.term_doc_dictionary
+        uniques = []
+        max_tf = 0
+        tf = {}
         # Go over each term in the doc
         for term in document_dictionary.keys():
+            #TODO: improve inv index algo
+            #TODO: chunk indexing
+            document.tf[term] += 1  # tf count in doc
             try:
                 # Update inverted index and posting
                 if term not in self.inverted_idx.keys():
                     self.inverted_idx[term] = 1
                     self.postingDict[term] = []
+                    uniques.append(term)
                 else:
-                    self.inverted_idx[term] += 1
-
-                self.postingDict[term].append((document.tweet_id, document_dictionary[term]))
+                    self.inverted_idx[term] += 1    # inverted_idx=DF
+                    uniques.remove(term)
+                    if self.inverted_idx[term] > max_tf:
+                        document.max_tf = self.inverted_idx[term]
+                document.unique_terms = uniques
+                self.postingDict[term].append(document)     # postingDict[term] = list of documents it appears
 
             except:
                 print('problem with the following key {}'.format(term[0]))
